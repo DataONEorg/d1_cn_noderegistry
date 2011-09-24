53ca409b3b81edebe2d07a428af43899654c0021
Robert Waltz <rwaltz@utk.edu>
2011-09-24 06:30:28 +0000

    updated for  using NodeReference as the key for operations and testing

--- src/main/java/org/dataone/cn/hazelcast/ldap/HazelcastLdapStore.java
+++ src/main/java/org/dataone/cn/hazelcast/ldap/HazelcastLdapStore.java
@@ -12,39 +12,38 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dataone.service.cn.impl.v1.NodeRegistryService;
+import org.dataone.service.exceptions.IdentifierNotUnique;
 import org.dataone.service.exceptions.NotFound;
 
 import org.dataone.service.exceptions.NotImplemented;
 import org.dataone.service.exceptions.ServiceFailure;
 import org.dataone.service.types.v1.Node;
 import org.dataone.service.types.v1.NodeList;
-
+import org.dataone.service.types.v1.NodeReference;
 
 /**
  * Provide read and write access to the nodelist stored in LDAP
  * @author waltz
  */
-
 public class HazelcastLdapStore implements MapLoader, MapStore {
 
     public static Log log = LogFactory.getLog(HazelcastLdapStore.class);
-
     private static NodeRegistryService nodeRegistry = new NodeRegistryService();
 
     @Override
     public Object load(Object key) {
         Node node = null;
-            try {
-                node = nodeRegistry.getNode((String) key);
-            } catch (NotFound ex) {
-                log.error(ex.serialize(NotImplemented.FMT_XML));
-        } catch (NotImplemented ex) {
-            log.error(ex.serialize(NotImplemented.FMT_XML));
+        try {
+            node = nodeRegistry.getNode((NodeReference) key);
+        } catch (NotFound ex) {
+            log.warn(ex.serialize(NotImplemented.FMT_XML));
         } catch (ServiceFailure ex) {
-            log.error(ex.serialize(NotImplemented.FMT_XML));
+            throw new RuntimeException(ex.getMessage());
         }
         return node;
     }
@@ -52,18 +51,20 @@ public class HazelcastLdapStore implements MapLoader, MapStore {
     @Override
     public Map loadAll(Collection keyCollection) {
         // Interpret loadAll as a way to get allNode again?
-        NodeList nodeList = null;
+        NodeList nodeList = new NodeList();
         try {
             nodeList = nodeRegistry.listNodes();
         } catch (NotImplemented ex) {
             log.error(ex.serialize(NotImplemented.FMT_XML));
+            throw new RuntimeException(ex.getMessage());
         } catch (ServiceFailure ex) {
             log.error(ex.serialize(NotImplemented.FMT_XML));
+            throw new RuntimeException(ex.getMessage());
         }
         List<Node> d1Nodes = nodeList.getNodeList();
-        Map<String, Node> mappedStore = new ConcurrentHashMap<String, Node>();
+        Map<NodeReference, Node> mappedStore = new ConcurrentHashMap<NodeReference, Node>();
         for (Node d1Node : d1Nodes) {
-            mappedStore.put(d1Node.getIdentifier().getValue(), d1Node);
+            mappedStore.put(d1Node.getIdentifier(), d1Node);
         }
         return mappedStore;
     }
@@ -72,45 +73,63 @@ public class HazelcastLdapStore implements MapLoader, MapStore {
     public Set loadAllKeys() {
         // upon instantiation of this Store, or when the first time
         // a map is called, loadAllKeys will be called.
-        NodeList nodeList = null;
+        NodeList nodeList = new NodeList();
         try {
             nodeList = nodeRegistry.listNodes();
         } catch (NotImplemented ex) {
             log.error(ex.serialize(NotImplemented.FMT_XML));
+            throw new RuntimeException(ex.getMessage());
         } catch (ServiceFailure ex) {
             log.error(ex.serialize(NotImplemented.FMT_XML));
+            throw new RuntimeException(ex.getMessage());
         }
         List<Node> d1Nodes = nodeList.getNodeList();
-        Set<String> keys = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+        Set<NodeReference> keys = Collections.newSetFromMap(new ConcurrentHashMap<NodeReference, Boolean>());
         for (Node d1Node : d1Nodes) {
-            keys.add(d1Node.getIdentifier().getValue());
+            keys.add(d1Node.getIdentifier());
         }
         return keys;
     }
 
     @Override
     public void store(Object key, Object value) {
+
+        // determine if the key already exists, if not create
+        // if so, then update
+        Node node = null;
         try {
-            nodeRegistry.updateLastHarvested((String) key, (Node) value);
+            node = nodeRegistry.getNode((NodeReference) key);
         } catch (ServiceFailure ex) {
-           log.error(ex.serialize(NotImplemented.FMT_XML));
+            throw new RuntimeException(ex.getMessage());
+        } catch (NotFound ex) {
+            log.info("Not found, so create a new node");
         }
+        try {
+            if (node == null) {
+                nodeRegistry.register((Node) value);
+            } else {
+                // update, but no idea what the update will look like
+                log.warn("unable to update currently");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(HazelcastLdapStore.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     @Override
     public void storeAll(Map map) {
-                try {
-        for (Object key : map.keySet()) {
-            nodeRegistry.updateLastHarvested((String) key, (Node) map.get(key));
-        }
-       } catch (ServiceFailure ex) {
-           log.error(ex.serialize(NotImplemented.FMT_XML));
-        }
+        throw new UnsupportedOperationException("Not supported yet.");
+
     }
 
     @Override
     public void delete(Object key) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            nodeRegistry.deleteNode((NodeReference) key);
+        } catch (ServiceFailure ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
     }
 
     @Override
