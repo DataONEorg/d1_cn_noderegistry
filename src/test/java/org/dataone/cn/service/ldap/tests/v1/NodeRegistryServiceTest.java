a8effa8fbe54e811ee54f2a77abc8fa84cd3424b
Robert Waltz <rwaltz@utk.edu>
2011-10-28 17:02:52 +0000

    #1917 Update CN Node Registry to 1.0.0

--- src/test/java/org/dataone/cn/service/ldap/tests/v1/NodeRegistryServiceTest.java
+++ src/test/java/org/dataone/cn/service/ldap/tests/v1/NodeRegistryServiceTest.java
@@ -1,5 +1,6 @@
 package org.dataone.cn.service.ldap.tests.v1;
 
+import org.dataone.service.types.v1.Subject;
 import org.dataone.service.types.v1.Services;
 import org.dataone.service.types.v1.Service;
 import org.dataone.service.types.v1.Schedule;
@@ -37,9 +38,15 @@ public class NodeRegistryServiceTest {
     @Test
     public void testRegisterAndDeleteNode() throws Exception {
         List<Node> testNodeList = new ArrayList<Node>();
-        Node testCNNode = new Node();
         NodeReference nodeId = new NodeReference();
         nodeId.setValue("test-1");
+        try {
+        nodeRegistryService.deleteNode(nodeId);
+        } catch (Exception e) {
+            // its ok, only would be there if there were a prior failure
+        }
+        Node testCNNode = new Node();
+
         testCNNode.setIdentifier(nodeId);
         testCNNode.setName("testThisCN");
         testCNNode.setDescription("this is a test");
@@ -48,6 +55,9 @@ public class NodeRegistryServiceTest {
         testCNNode.setSynchronize(false);
         testCNNode.setType(NodeType.CN);
         testCNNode.setState(NodeState.UP);
+        Subject cncontactSubject = new Subject();
+        cncontactSubject.setValue("cn=test,dc=dataone,dc=org");
+        testCNNode.addContactSubject(cncontactSubject);
         NodeReference cnNodeReference = nodeRegistryService.register(testCNNode);
         assertNotNull(cnNodeReference);
         testNodeList.add(testCNNode);
@@ -55,6 +65,12 @@ public class NodeRegistryServiceTest {
         Node testMNNode = new Node();
         NodeReference mnodeId = new NodeReference();
         mnodeId.setValue("test-2");
+        try {
+        nodeRegistryService.deleteNode(mnodeId);
+        } catch (Exception e) {
+            // its ok, only would be there if there were a prior failure
+        }
+
         testMNNode.setIdentifier(mnodeId);
         testMNNode.setName("testThisMN");
         testMNNode.setDescription("this is a test");
@@ -64,7 +80,9 @@ public class NodeRegistryServiceTest {
         testMNNode.setType(NodeType.MN);
         testMNNode.setState(NodeState.UP);
         Synchronization synchronization = new Synchronization();
-
+        Subject mncontactSubject = new Subject();
+        mncontactSubject.setValue("cn=test,dc=dataone,dc=org");
+        testMNNode.addContactSubject(mncontactSubject);
         synchronization.setLastCompleteHarvest(new Date());
         synchronization.setLastHarvested(new Date());
         Schedule schedule = new Schedule();
@@ -94,6 +112,7 @@ public class NodeRegistryServiceTest {
         services.addService(service2);
 
         testMNNode.setServices(services);
+
         NodeReference mnNodeReference = nodeRegistryService.register(testMNNode);
         assertNotNull(mnNodeReference);
         testNodeList.add(testMNNode);
