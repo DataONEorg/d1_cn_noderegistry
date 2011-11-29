292a21288b19b552165786214292feeb40685f62
Robert Waltz <rwaltz@utk.edu>
2011-11-29 17:06:47 +0000

    Story #1849 Node Schema in LDAP needs an approval field Story #1964 node registry needs to store Node.Service.ServiceMethodRestriction Story #1757 Implement CNRegister.updateNodeCapabilities

--- src/main/java/org/dataone/cn/hazelcast/ldap/HazelcastLdapStore.java
+++ src/main/java/org/dataone/cn/hazelcast/ldap/HazelcastLdapStore.java
@@ -18,6 +18,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dataone.service.cn.impl.v1.NodeRegistryService;
 import org.dataone.service.exceptions.IdentifierNotUnique;
+import org.dataone.service.exceptions.InvalidRequest;
 import org.dataone.service.exceptions.NotFound;
 
 import org.dataone.service.exceptions.NotImplemented;
@@ -43,6 +44,7 @@ public class HazelcastLdapStore implements MapLoader, MapStore {
         } catch (NotFound ex) {
             log.warn(ex.serialize(NotImplemented.FMT_XML));
         } catch (ServiceFailure ex) {
+            ex.printStackTrace();
             throw new RuntimeException(ex.getMessage());
         }
         return node;
@@ -58,7 +60,7 @@ public class HazelcastLdapStore implements MapLoader, MapStore {
             log.error(ex.serialize(NotImplemented.FMT_XML));
             throw new RuntimeException(ex.getMessage());
         } catch (ServiceFailure ex) {
-            log.error(ex.serialize(NotImplemented.FMT_XML));
+            log.error(ex.serialize(ServiceFailure.FMT_XML));
             throw new RuntimeException(ex.getMessage());
         }
         List<Node> d1Nodes = nodeList.getNodeList();
@@ -80,7 +82,7 @@ public class HazelcastLdapStore implements MapLoader, MapStore {
             log.error(ex.serialize(NotImplemented.FMT_XML));
             throw new RuntimeException(ex.getMessage());
         } catch (ServiceFailure ex) {
-            log.error(ex.serialize(NotImplemented.FMT_XML));
+            log.error(ex.serialize(ServiceFailure.FMT_XML));
             throw new RuntimeException(ex.getMessage());
         }
         List<Node> d1Nodes = nodeList.getNodeList();
@@ -94,25 +96,56 @@ public class HazelcastLdapStore implements MapLoader, MapStore {
     @Override
     public void store(Object key, Object value) {
 
-        // determine if the key already exists, if not create
+        // Hazelcast can not use store to create an Node
+        // Since keys are randomly assigned at creation,
+        // any key provided by store will be lost, and the
+        // key stored in the hazelcast map will be incorrect
+
+        // furthermore, when a node is created, you should not be
+        // able to 'get' the node via hazelcast map
+        // until it has been approved
+        // if store where able to create a node, then it
+        // would be possible to get the node before
+        // it is approved
+
+        // determine if the key already exists, 
+        // if not, then throw an exception
         // if so, then update
+        NodeReference nodeId = (NodeReference) key;
         Node node = null;
         try {
-            node = nodeRegistry.getNode((NodeReference) key);
+            node = nodeRegistry.getNode(nodeId);
         } catch (ServiceFailure ex) {
             throw new RuntimeException(ex.getMessage());
         } catch (NotFound ex) {
-            log.info("Not found, so create a new node");
+            log.error("Node Not Found, a node must be created through Node Registry Service and approved before Hazelcast can update it");
+            throw new RuntimeException(ex.getMessage());
         }
-        try {
-            if (node == null) {
-                nodeRegistry.register((Node) value);
-            } else {
+
+        if (node == null) {
+            throw new RuntimeException("Node is null, a node must be created through Node Registry Service and approved before Hazelcast can update it");
+        } else {
+            try {
                 // update, but no idea what the update will look like
-                log.warn("unable to update currently");
+                if (nodeRegistry.updateNodeCapabilities(nodeId, (Node) value)) {
+                    log.debug("updated NodeCapabilities for " + nodeId.getValue());
+                } else {
+                    log.error("Failed to update NodeCapabilities for " + nodeId.getValue());
+                    throw new RuntimeException("Node is null, a node must be created through Node Registry Service and approved before Hazelcast can update it");
+                }
+            } catch (NotImplemented ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex.getMessage());
+            } catch (ServiceFailure ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex.getMessage());
+            } catch (InvalidRequest ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex.getMessage());
+            } catch (NotFound ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex.getMessage());
             }
-        } catch (Exception ex) {
-            Logger.getLogger(HazelcastLdapStore.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
