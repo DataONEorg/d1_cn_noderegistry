3a570eaf5343f75ddd3d1c77e780b1ce6a4a823d
Robert Waltz <rwaltz@utk.edu>
2012-10-09 22:49:08 +0000

    When modifying a node, and a sync schedule is added, LastHarvested and LastCompleteHarvest were not being added

--- src/test/java/org/dataone/cn/service/ldap/tests/v1/NodeRegistryServiceTest.java
+++ src/test/java/org/dataone/cn/service/ldap/tests/v1/NodeRegistryServiceTest.java
@@ -1,25 +1,20 @@
 /**
- * This work was created by participants in the DataONE project, and is
- * jointly copyrighted by participating institutions in DataONE. For 
- * more information on DataONE, see our web site at http://dataone.org.
+ * This work was created by participants in the DataONE project, and is jointly copyrighted by participating
+ * institutions in DataONE. For more information on DataONE, see our web site at http://dataone.org.
  *
- *   Copyright ${year}
+ * Copyright ${year}
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
- * limitations under the License.
- * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
  * $Id$
  */
-
 package org.dataone.cn.service.ldap.tests.v1;
 
 import java.io.IOException;
@@ -45,19 +40,18 @@ import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.Date;
 import javax.naming.NamingException;
 import javax.naming.directory.DirContext;
 import org.dataone.cn.ldap.NodeAccess;
 import org.dataone.cn.ldap.NodeServicesAccess;
 import org.dataone.cn.ldap.ServiceMethodRestrictionsAccess;
 import org.dataone.service.exceptions.NotImplemented;
-import org.dataone.service.types.v1.Service;
-import org.dataone.service.types.v1.ServiceMethodRestriction;
-import org.dataone.service.types.v1.Services;
+import org.dataone.service.types.v1.*;
 import org.junit.Before;
 
 public class NodeRegistryServiceTest {
-
+    
     public static Log log = LogFactory.getLog(NodeRegistryServiceTest.class);
     NodeRegistryService nodeRegistryService = new NodeRegistryService();
     NodeAccess nodeAccess = new NodeAccess();
@@ -68,6 +62,7 @@ public class NodeRegistryServiceTest {
     Node testMNNode;
     Node testCNNode;
     Node testMNNoSynchNode;
+
     @Before
     public void removeAnyTestNodes() throws IOException, InstantiationException, IllegalAccessException, JiBXException, NamingException {
         testMNNode = buildTestNode("/org/dataone/cn/resources/samples/v1/mnNode.xml");
@@ -78,28 +73,29 @@ public class NodeRegistryServiceTest {
         ldapPopulationService.deleteTestNodesByName(testCNNode.getIdentifier().getValue());
         ldapPopulationService.deleteTestNodesByName(testMNNoSynchNode.getIdentifier().getValue());
     }
+
     @Test
     public void testRegisterListAndDeleteNode() throws Exception {
         List<Node> testNodeList = new ArrayList<Node>();
-
+        
         NodeReference mnNodeReference = nodeRegistryService.register(testMNNode);
         assertNotNull(mnNodeReference);
         testNodeList.add(testMNNode);
-
+        
         NodeReference cnNodeReference = nodeRegistryService.register(testCNNode);
         testCNNode.setIdentifier(cnNodeReference);
         testNodeList.add(testCNNode);
-
+        
         Node testCNRetrieval = nodeRegistryService.getNode(cnNodeReference);
-
+        
         ByteArrayOutputStream outputTestCNStream = new ByteArrayOutputStream();
         TypeMarshaller.marshalTypeToOutputStream(testCNRetrieval, outputTestCNStream);
         String testCNNodeString = new String(outputTestCNStream.toByteArray());
         log.info(testCNNodeString);
-
+        
         assertTrue(testCNRetrieval.getIdentifier().getValue().equalsIgnoreCase(cnNodeReference.getValue()));
         for (Node node : testNodeList) {
-
+            
             nodeAccess.setNodeApproved(node.getIdentifier(), Boolean.TRUE);
         }
         try {
@@ -125,7 +121,7 @@ public class NodeRegistryServiceTest {
                                 }
                             }
                         }
-
+                        
                     }
                 }
             }
@@ -140,9 +136,9 @@ public class NodeRegistryServiceTest {
             e.printStackTrace();
             fail("testNodeList");
         }
-
+        
         for (Node node : testNodeList) {
-
+            
             List<Service> services = nodeServicesAccess.getServiceList(node.getIdentifier().getValue());
             if ((services != null) && (services.size() > 0)) {
                 for (Service service : services) {
@@ -152,61 +148,95 @@ public class NodeRegistryServiceTest {
                         for (ServiceMethodRestriction restriction : serviceRestrictionList) {
                             log.debug("deleteNode deleting " + serviceMethodRestrictionsAccess.buildServiceMethodRestrictionDN(node.getIdentifier(), service, restriction));
                             if (!serviceMethodRestrictionsAccess.deleteServiceMethodRestriction(node.getIdentifier(), service, restriction)) {
-
-                                fail( "Unable to delete restriction " + serviceMethodRestrictionsAccess.buildServiceMethodRestrictionDN(node.getIdentifier(), service, restriction));
+                                
+                                fail("Unable to delete restriction " + serviceMethodRestrictionsAccess.buildServiceMethodRestrictionDN(node.getIdentifier(), service, restriction));
                             }
                         }
                     }
                     if (!nodeServicesAccess.deleteNodeService(node.getIdentifier(), service)) {
-                        fail(  "Unable to delete service " + nodeServicesAccess.buildNodeServiceDN(node.getIdentifier(), service));
+                        fail("Unable to delete service " + nodeServicesAccess.buildNodeServiceDN(node.getIdentifier(), service));
                     }
                 }
             }
             nodeAccess.deleteNode(node.getIdentifier());
         }
     }
+
     @Test
     public void testRegisterNoSyncMNNode() throws Exception {
         // This should be able to register without error, that is all
-        NodeReference cnNodeReference = nodeRegistryService.register(testMNNoSynchNode);
+        NodeReference mnNodeReference = nodeRegistryService.register(testMNNoSynchNode);
         ldapPopulationService.deleteTestNodesByName(testMNNoSynchNode.getIdentifier().getValue());
     }
-    @Test(expected=InvalidRequest.class)
-    public void testRegisterBadLocalhostNode() throws IOException, InstantiationException, IllegalAccessException, JiBXException, ServiceFailure, IdentifierNotUnique, InvalidRequest, NotImplemented  {
+
+    @Test
+    public void testUpdateNoSyncNodeWithSyncMNNode() throws Exception {
+        // This should be able to register without error, that is all
+        NodeReference mnNodeReference = nodeRegistryService.register(testMNNoSynchNode);
+        Node mnRegisteredNode = nodeRegistryService.getNode(mnNodeReference);
+        Synchronization sync = new Synchronization();
+        Schedule sched = new Schedule();
+        sched.setSec("30");
+        sched.setMin("30");
+        sched.setHour("01");
+        sched.setMday("*");
+        sched.setMon("*");
+        sched.setWday("?");
+        sched.setYear("*");
+        sync.setSchedule(sched);
+        sync.setLastHarvested(new Date());
+        sync.setLastCompleteHarvest(new Date());
+        mnRegisteredNode.setSynchronization(sync);
+        mnRegisteredNode.setSynchronize(true);
+        nodeRegistryService.updateNodeCapabilities(mnNodeReference, mnRegisteredNode);
+        mnRegisteredNode = nodeRegistryService.getNode(mnNodeReference);
+        try {
+            assertTrue(mnRegisteredNode.getSynchronization().getSchedule() != null);
+        } catch (NullPointerException ex) {
+            fail("Test misconfiguration " + ex);
+        }
+        ldapPopulationService.deleteTestNodesByName(testMNNoSynchNode.getIdentifier().getValue());
+    }
+
+    @Test(expected = InvalidRequest.class)
+    public void testRegisterBadLocalhostNode() throws IOException, InstantiationException, IllegalAccessException, JiBXException, ServiceFailure, IdentifierNotUnique, InvalidRequest, NotImplemented {
         Node testMNNode = buildTestNode("/org/dataone/cn/resources/samples/v1/mnBadLocalhostNode.xml");
-
+        
         NodeReference mnNodeReference = nodeRegistryService.register(testMNNode);
-
+        
     }
-    @Test(expected=InvalidRequest.class)
-    public void testRegisterBadNodeId() throws IOException, InstantiationException, IllegalAccessException, JiBXException, ServiceFailure, IdentifierNotUnique, InvalidRequest, NotImplemented  {
 
-        Node testMNNode =buildTestNode("/org/dataone/cn/resources/samples/v1/mnBadNodeId.xml");
-
+    @Test(expected = InvalidRequest.class)
+    public void testRegisterBadNodeId() throws IOException, InstantiationException, IllegalAccessException, JiBXException, ServiceFailure, IdentifierNotUnique, InvalidRequest, NotImplemented {
+        
+        Node testMNNode = buildTestNode("/org/dataone/cn/resources/samples/v1/mnBadNodeId.xml");
+        
         NodeReference mnNodeReference = nodeRegistryService.register(testMNNode);
-
+        
     }
-    @Test(expected=InvalidRequest.class)
-    public void testRegisterBadSyncNode() throws IOException, InstantiationException, IllegalAccessException, JiBXException, ServiceFailure, IdentifierNotUnique, InvalidRequest, NotImplemented  {
 
-        Node testMNNode =buildTestNode("/org/dataone/cn/resources/samples/v1/mnNodeFailOnSynchronization.xml");
-
+    @Test(expected = InvalidRequest.class)
+    public void testRegisterBadSyncNode() throws IOException, InstantiationException, IllegalAccessException, JiBXException, ServiceFailure, IdentifierNotUnique, InvalidRequest, NotImplemented {
+        
+        Node testMNNode = buildTestNode("/org/dataone/cn/resources/samples/v1/mnNodeFailOnSynchronization.xml");
+        
         NodeReference mnNodeReference = nodeRegistryService.register(testMNNode);
+        
+    }    
 
-    } 
-    @Test(expected=InvalidRequest.class)
-    public void testRegisterBadSyncScheduleNode() throws IOException, InstantiationException, IllegalAccessException, JiBXException, ServiceFailure, IdentifierNotUnique, InvalidRequest, NotImplemented  {
-
-        Node testMNNode =buildTestNode("/org/dataone/cn/resources/samples/v1/mnNodeFailOnSchedule.xml");
-
+    @Test(expected = InvalidRequest.class)
+    public void testRegisterBadSyncScheduleNode() throws IOException, InstantiationException, IllegalAccessException, JiBXException, ServiceFailure, IdentifierNotUnique, InvalidRequest, NotImplemented {
+        
+        Node testMNNode = buildTestNode("/org/dataone/cn/resources/samples/v1/mnNodeFailOnSchedule.xml");
+        
         NodeReference mnNodeReference = nodeRegistryService.register(testMNNode);
-
+        
     }
-   
+    
     private Node buildTestNode(String resourcePath) throws IOException, InstantiationException, IllegalAccessException, JiBXException {
         ByteArrayOutputStream mnNodeOutput = new ByteArrayOutputStream();
         InputStream is = this.getClass().getResourceAsStream(resourcePath);
-
+        
         BufferedInputStream bInputStream = new BufferedInputStream(is);
         byte[] barray = new byte[SIZE];
         int nRead = 0;
